64fcb51da222002ad7668a9714ffacf9a538914e
MaciejHoroszkiewicz <horoszkiewicz.maciej@gmail.com>
2023-02-28 01:38:42 +0100

    Added function

--- calculator.py
+++ calculator.py
@@ -1,40 +1,43 @@
 import logging 
 logging.basicConfig(level=logging.DEBUG)
 
+
 #def calculator(x, y):
-calculate = input("Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2: Odejmowanie, 3: Mnożenie, 4: Dzielenie\n")
-if calculate == "1":
+def calculate(operation):
     x = input("Podaj składnik 1: ")
     y = input("Podaj składnik 2: ")
-    logging.info(f"Dodaję {x} do {y}")
-    if "." in x or "." in y:
-        print(f"Wynik to {float(x) + float(y)}")
-    else:
-        print(f"Wynik to {int(x) + int(y)}")
-elif calculate == "2":
-    x = input("Podaj składnik 1: ")
-    y = input("Podaj składnik 2: ")
-    logging.info(f"Odejmuję {x} od {y}.")
-    if "." in x or "." in y:
-        print(f"Wynik to {float(x) - float(y)}")
-    else:
-        print(f"Wynik to {int(x) - (int(y))}")
-elif calculate == "3":
-    x = input("Podaj składnik 1: ")
-    y = input("Podaj składnik 2: ")
-    logging.info(f"Mnożę {x} i {y}")
-    if "." in x or "." in y:
-        print(f"Wynik to {float(x) * float(y)}")
-    else:
-        print(f"Wynik to {int(x) * int(y)}")
-elif calculate == "4":
-    x = input("Podaj składnik 1: ")
-    y = input("Podaj składnik 2: ")
-    logging.info(f"Dzielę {x} przez {y}")
-    if y == "0":
-        print("Nie można dzielić przez zero!")
-    else:
+
+    if operation == "1":
+        logging.info(f"Dodaję {x} do {y}")
         if "." in x or "." in y:
-            print(f"Wynik to {float(x) // float(y)}")
+            result = float(x) + float(y)
         else:
-            print(f"Wynik to {int(x) // int(y)}")
+            result = int(x) + int(y)
+    elif operation == "2":
+        logging.info(f"Odejmuję {x} od {y}.")
+        if "." in x or "." in y:
+            result = float(x) - float(y)
+        else:
+            result = int(x) - int(y)
+    elif operation == "3":
+        logging.info(f"Mnożę {x} i {y}")
+        if "." in x or "." in y:
+            result = float(x) * float(y)
+        else:
+            result = int(x) * int(y)
+    elif operation == "4":
+        logging.info(f"Dzielę {x} przez {y}")
+        if y == "0":
+            logging.warning("Nie można dzielić przez zero!")
+            result = None
+        else:
+            if "." in x or "." in y:
+                result = float(x) // float(y)
+            else:
+                result = int(x) // int(y)
+
+    return result
+
+operation = input("Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2: Odejmowanie, 3: Mnożenie, 4: Dzielenie\n")
+result = calculate(operation)
+print(f"Wynik to {result}.")
